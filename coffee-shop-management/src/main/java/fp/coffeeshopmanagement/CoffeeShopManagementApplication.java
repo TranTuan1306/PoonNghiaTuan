ee465395ed254a8434396b50f62c5fd765aa4bc8
Poon Nguyễn <poonnguyen1998@outlook.com>
2018-12-11 16:19:29 +0700

    Post project

---
+++ coffee-shop-management/src/main/java/fp/coffeeshopmanagement/CoffeeShopManagementApplication.java
@@ -0,0 +1,12 @@
+package fp.coffeeshopmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeShopManagementApplication.class, args);
+	}
+}
