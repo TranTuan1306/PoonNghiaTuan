244cf53622b1ef8829bc84d11decf9b3037f5abe
Poon Nguyễn <poonnguyen1998@outlook.com>
2018-12-13 12:37:21 +0700

    ADD REPOSITORY </br> ChiTietHopDongCungCapNguyenLieuRepository
    Signed-off-by: Poon Nguyễn <poonnguyen1998@outlook.com>

---
+++ FinalProject/src/main/java/fp/coffeeshopmanagement/repository/ChiTietHopDongCungCapNguyenLieuRepository.java
@@ -0,0 +1,8 @@
+package fp.coffeeshopmanagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import fp.coffeeshopmanagement.model.ChiTietHopDongCungCapNguyenLieu;
+
+public interface ChiTietHopDongCungCapNguyenLieuRepository extends CrudRepository<ChiTietHopDongCungCapNguyenLieu, Integer> {
+
+}
