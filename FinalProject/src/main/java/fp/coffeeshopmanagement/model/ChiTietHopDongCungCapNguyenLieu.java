ced67811c9eeb771e97f62977b760eae5843763b
Poon Nguyễn <poonnguyen1998@outlook.com>
2018-12-13 19:41:20 +0700

    ADD SERVICE IMPLEMENTS ChiTietHoaDon ChiTietHopDongCungCapNguyenLieu ChiTietNguyenLieu CongViec HoaDon HopDongCungCapNguyenLieu ChiTietHopDongCungCapNguyenLieu

--- FinalProject/src/main/java/fp/coffeeshopmanagement/model/ChiTietHopDongCungCapNguyenLieu.java
+++ FinalProject/src/main/java/fp/coffeeshopmanagement/model/ChiTietHopDongCungCapNguyenLieu.java
@@ -7,6 +7,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name="chitiethopdongcungcapnguyenlieu")
 public class ChiTietHopDongCungCapNguyenLieu {
+	
 	@Column(name="mahopdong")
 	private int iMaHopDong;
 	
---
+++ FinalProject/src/main/java/fp/coffeeshopmanagement/service/CongViecImpl.java
@@ -0,0 +1,39 @@
+package fp.coffeeshopmanagement.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fp.coffeeshopmanagement.model.CongViec;
+import fp.coffeeshopmanagement.repository.CongViecRepository;
+
+@Service
+@Transactional
+public class CongViecImpl implements CongViecService{
+	@Autowired
+	CongViecRepository cvRepository;
+
+	public List<CongViec> getAllCongViec() {
+		// TODO Auto-generated method stub
+		return (List<CongViec>)cvRepository.findAll();
+	}
+
+	public CongViec getCongViecById(int id) {
+		// TODO Auto-generated method stub
+		return cvRepository.findById(id).get();
+	}
+
+	public void saveOrUpdate(CongViec cv) {
+		// TODO Auto-generated method stub
+		cvRepository.save(cv);
+	}
+
+	public void deleteCongViec(int id) {
+		// TODO Auto-generated method stub
+		cvRepository.deleteById(id);
+	}
+	
+	
+}
